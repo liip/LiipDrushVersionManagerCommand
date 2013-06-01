a938840790517e11bb3bd6d9949697a365ce6306
Bastian Feder <bastian.feder@liip.ch>
2013-06-01 12:13:20 +0200

    Added test suite

---
+++ tests/vmTest.php
@@ -0,0 +1,26 @@
+<?php
+
+class VmTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider getModulesFromArgumentsDataprovider
+     */
+    public function testGetModulesFromArguments($modules, $expected)
+    {
+        $this->assertEquals($expected, _drush_vm_get_modules_from_arguments($modules));
+    }
+    public static function getModulesFromArgumentsDataprovider()
+    {
+        return array(
+            'no modules (just commas) provided' => array(',,,', array()),
+            'no modules provided' => array('', array()),
+            'one module provided' => array('tux', array('tux')),
+            'multiple modules provided' => array('tux, devil', array('tux', 'devil')),
+        );
+    }
+
+    public function testHasOption()
+    {
+        $this->assertEquals(true)
+    }
+}
